cdb2f34e21729ffbd1bf17d915b70540aec39dcd
Behnam <benyblack@gmail.com>
2019-08-09 13:49:03 +0200

    Add SMA method with tests

--- src/talib/indicators/MA.cs
+++ src/talib/indicators/MA.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Linq;
 
 namespace Talib.Indicators {
     public class MA {
@@ -7,12 +7,20 @@ namespace Talib.Indicators {
             if (period > data.Length || period <= 0) {
                 return null;
             }
-            
+
             double sum = 0;
             for (int i = data.Length - 1; i >= data.Length - period; i--) {
                 sum += data[i];
             }
             return sum / period;
+        }
+
+        public double?[] Sma (double[] data, int period) {
+            var result = new double?[data.Length];
+            for (int i = 0; i < data.Length; i++) {
+                result[i] = SmaSingle(data.Take (i + 1).ToArray(), period);
+            }
+            return result;
         }
     }
 }
--- tests/talib.tests/MaTests.cs
+++ tests/talib.tests/MaTests.cs
@@ -1,41 +1,60 @@
 using System;
-using System.Collections.Generic;
-using Xunit;
+using System.Linq;
 using Talib.Indicators;
-
+using Xunit;
 
 namespace Talib.Tests {
     public class MaTests {
 
-        private readonly double[] data = {46.282, 45.614, 46.0328, 45.8931, 46.0826, 45.8433, 45.4245, 45.0955, 44.8264, 44.3278, 43.6124, 44.1497, 44.0902, 44.3389, 44.3189, 44.3289};
+        private readonly double[] data = { 46.282, 45.614, 46.0328, 45.8931, 46.0826, 45.8433, 45.4245, 45.0955, 44.8264, 44.3278, 43.6124, 44.1497, 44.0902, 44.3389, 44.3189, 44.3289 };
 
         [Fact]
         public void SmaSingle_SimpleValues_calculated () {
-            var ma = new MA();
-            
-            var result = Math.Round(ma.SmaSingle(data, 10).Value, 4) ;
+            var ma = new MA ();
 
-            Assert.Equal(44.4513D, result);
+            var result = Math.Round (ma.SmaSingle (data, 10).Value, 4);
+
+            Assert.Equal (44.4513D, result);
         }
 
         [Fact]
-        public void SmaSingle_PeriodBiggerThanDataSize_ReturnNull(){
-            var ma = new MA();
-            
-            var result = ma.SmaSingle(data, 1000);
+        public void SmaSingle_PeriodBiggerThanDataSize_ReturnNull () {
+            var ma = new MA ();
 
-            Assert.Null(result);
+            var result = ma.SmaSingle (data, 1000);
+
+            Assert.Null (result);
         }
 
         [Fact]
-        public void SmaSingle_PeriodZeroOrLess_ReturnNull(){
-            var ma = new MA();
+        public void SmaSingle_PeriodZeroOrLess_ReturnNull () {
+            var ma = new MA ();
 
-            var result = ma.SmaSingle(data, 0);
-            var result2 = ma.SmaSingle(data, -10);
+            var result = ma.SmaSingle (data, 0);
+            var result2 = ma.SmaSingle (data, -10);
 
-            Assert.Null(result);
-            Assert.Null(result2);
+            Assert.Null (result);
+            Assert.Null (result2);
+        }
+
+        [Fact]
+        public void Sma_SimpleValues_calculated () {
+            var ma = new MA ();
+
+            double[] result = ma.Sma (data, 3).Where (x => x != null).Select (x => Math.Round (x.Value, 4)).ToArray ();
+            int nullCounts = ma.Sma (data, 3).Where (x => x == null).Count ();
+            double[] expected = { 45.9763, 45.8466, 46.0028, 45.9397, 45.7835, 45.4544, 45.1155, 44.7499, 44.2555, 44.03, 43.9508, 44.1929, 44.2493, 44.3289 };
+            Assert.Equal (expected, result);
+            Assert.Equal (2, nullCounts);
+        }
+
+        [Fact]
+        public void Sma_PeriodBiggerThanDataSize_ReturnNull () {
+            var ma = new MA ();
+
+            double?[] result = ma.Sma (data, 300);
+            double?[] expected = { null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null };
+            Assert.Equal (expected, result);
         }
     }
 }
