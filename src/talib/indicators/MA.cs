531464fa1c176888dc104fbf05f241508da930a8
Behnam <benyblack@gmail.com>
2019-08-09 13:19:53 +0200

    Add SingleSma method with tests

--- src/talib/indicators/MA.cs
+++ src/talib/indicators/MA.cs
@@ -1,7 +1,18 @@
-namespace Talib.Indicators
-{
-    public class MA
-    {
-        
+using System;
+
+namespace Talib.Indicators {
+    public class MA {
+
+        public double? SmaSingle (double[] data, int period) {
+            if (period > data.Length || period <= 0) {
+                return null;
+            }
+            
+            double sum = 0;
+            for (int i = data.Length - 1; i >= data.Length - period; i--) {
+                sum += data[i];
+            }
+            return sum / period;
+        }
     }
 }
---
+++ tests/talib.tests/MaTests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Talib.Indicators;
+
+
+namespace Talib.Tests {
+    public class MaTests {
+
+        private readonly double[] data = {46.282, 45.614, 46.0328, 45.8931, 46.0826, 45.8433, 45.4245, 45.0955, 44.8264, 44.3278, 43.6124, 44.1497, 44.0902, 44.3389, 44.3189, 44.3289};
+
+        [Fact]
+        public void SMA_SimpleValues_calculated () {
+            var ma = new MA();
+            
+            var result = Math.Round(ma.SmaSingle(data, 10).Value, 4) ;
+
+            Assert.Equal(44.4513D, result);
+        }
+
+        [Fact]
+        public void SMA_PeriodBiggerThanDataSize_ReturnNull(){
+            var ma = new MA();
+            
+            var result = ma.SmaSingle(data, 1000);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void SMA_PeriodZeroOrLess_ReturnNull(){
+            var ma = new MA();
+
+            var result = ma.SmaSingle(data, 0);
+            var result2 = ma.SmaSingle(data, -10);
+
+            Assert.Null(result);
+            Assert.Null(result2);
+        }
+    }
+}
