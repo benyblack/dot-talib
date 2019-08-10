f4b8c484f6a9a308cf34bfa59b6521fa3c052124
Behnam <benyblack@gmail.com>
2019-08-10 23:03:45 +0200

    Add ema and emaSingle methods
    Unit test fails for Ema, needs to be fixed.

--- src/talib/indicators/MA.cs
+++ src/talib/indicators/MA.cs
@@ -1,26 +1,95 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace Talib.Indicators {
-    public class MA {
+namespace Talib.Indicators
+{
+    public class MA
+    {
 
-        public double? SmaSingle (double[] data, int period) {
-            if (period > data.Length || period <= 0) {
+        public double? SmaSingle(double[] data, int period)
+        {
+            if (period > data.Length || period <= 0)
+            {
                 return null;
             }
 
             double sum = 0;
-            for (int i = data.Length - 1; i >= data.Length - period; i--) {
+            for (int i = data.Length - 1; i >= data.Length - period; i--)
+            {
                 sum += data[i];
             }
             return sum / period;
         }
 
-        public double?[] Sma (double[] data, int period) {
+        public double?[] Sma(double[] data, int period)
+        {
             var result = new double?[data.Length];
-            for (int i = 0; i < data.Length; i++) {
-                result[i] = SmaSingle(data.Take (i + 1).ToArray(), period);
+            for (int i = 0; i < data.Length; i++)
+            {
+                result[i] = SmaSingle(data.Take(i + 1).ToArray(), period);
             }
             return result;
+        }
+
+        public double? EmaSingle(double[] data, int period)
+        {
+            if (data == null || period <= 0)
+            {
+                return null;
+            }
+            double multiplier = 2D / (period + 1);
+            Array.Reverse(data);
+            double? EmaSingleInner(double[] idata)
+            {
+                if (period > idata.Length)
+                {
+                    return null;
+                }
+                if (period == idata.Length)
+                {
+                    return SmaSingle(data, period);
+                }
+                double? last_ema = EmaSingleInner(idata.Skip(1).ToArray());
+                return last_ema + (multiplier * (idata.Take(1).Single() - last_ema));
+            }
+            return EmaSingleInner(data);
+        }
+
+        public double?[] Ema(double[] data, int period)
+        {
+            if (data == null || period <= 0)
+            {
+                return null;
+            }
+            double multiplier = 2D / (period + 1);
+            Array.Reverse(data);
+            List<double?> emaList = new List<double?>();
+            Dictionary<double[], double?> memo = new Dictionary<double[], double?>();
+            double? EmaInner(double[] idata)
+            {
+                if (period > idata.Length)
+                {
+                    return null;
+                }
+                if (period == idata.Length)
+                {
+                    memo[idata] = SmaSingle(data, period);
+                    return memo[idata];
+                }
+                var subData = idata.Skip(1).ToArray();
+                if (!memo.ContainsKey(subData))
+                {
+                    memo[subData] = EmaInner(subData);
+                }
+                double? last_ema = memo[subData];
+                var result = last_ema + (multiplier * (idata.Take(1).Single() - last_ema));
+                emaList.Add(last_ema);
+                return result;
+            }
+            EmaInner(data);
+            emaList.Reverse();
+            return emaList.ToArray();
         }
     }
 }
--- tests/talib.tests/MaTests.cs
+++ tests/talib.tests/MaTests.cs
@@ -7,9 +7,9 @@ namespace Talib.Tests {
     public class MaTests {
 
         private readonly double[] data = { 46.282, 45.614, 46.0328, 45.8931, 46.0826, 45.8433, 45.4245, 45.0955, 44.8264, 44.3278, 43.6124, 44.1497, 44.0902, 44.3389, 44.3189, 44.3289 };
-
+        private readonly double[] data2 = { 1208.2, 1279.15, 1317.55, 1318.15, 1306.95, 1305.15, 1308.05, 1307.4, 1297.7, 1287.65, 1291.65, 1322.9, 1350.7, 1342.7, 1341.2, 1345.35, 1348.7, 1356, 1399.1, 1386.3, 1408.7, 1389.2, 1382.05, 1372.7, 1378.35, 1395.9, 1396.75, 1399.95, 1429.9, 1438.65, 1430.1, 1399.65, 1333.95, 1334.1, 1314.15, 1302.7, 1302.15, 1295.3, 1309, 1313.3, 1336.75, 1319.2, 1319.25, 1308.95, 1303.65, 1301.8, 1299.4, 1299.6, 1300.6, 1345.6, 1307.35, 1293.2, 1287.45, 1286.7, 1276.1, 1281.1, 1309.15, 1293.2, 1258.35, 1254.6, 1235.15, 1258.9, 1264.65, 1272.3, 1272.2, 1273.3, 1257.75, 1259.9, 1266.5, 1255.15, 1233.8, 1253.15, 1250.2, 1258.2, 1246.9, 1228.5, 1242.95, 1250.5, 1245.7, 1248.3, 1246.55, 1235.75, 1225.05, 1227.05, 1244.5, 1233.7, 1244.55, 1270.7, 1266, 1266.2, 1254.25, 1296.95, 1309.6, 1303.8, 1305.75, 1319.6, 1338.7, 1340, 1334.65, 1330.95 };
         [Fact]
-        public void SmaSingle_SimpleValues_calculated () {
+        public void SmaSingle_SimpleValues_Calculated () {
             var ma = new MA ();
 
             var result = Math.Round (ma.SmaSingle (data, 10).Value, 4);
@@ -54,7 +54,71 @@ namespace Talib.Tests {
 
             double?[] result = ma.Sma (data, 300);
             double?[] expected = { null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null };
+
             Assert.Equal (expected, result);
+        }
+
+        [Fact]
+        public void EmaSingle_SimpleValues_Calculated () {
+            //Given
+            var ma = new MA ();
+
+            //When
+            var result = Math.Round(ma.EmaSingle(data2, 5).Value, 2);
+
+            //Then
+            Assert.Equal (1328.45D, result);
+        }
+
+        [Fact]
+        public void EmaSingle_PeriodBiggerThanDataLength_ReturnNull () {
+            //Given
+            var ma = new MA ();
+
+            //When
+            var result = ma.EmaSingle(data2, 1000);
+            
+            //Then
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void EmaSingle_NullData_ReturnNull () {
+            //Given
+            var ma = new MA ();
+
+            //When
+            var result = ma.EmaSingle(null, 1000);
+            
+            //Then
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void EmaSingle_PeriodZeroOrLess_ReturnNull () {
+            //Given
+            var ma = new MA();
+
+            //When
+            var result = ma.EmaSingle(data2, 0);
+            var result2 = ma.EmaSingle(data2, -10);
+            
+            //Then
+            Assert.Null(result);
+            Assert.Null(result2);
+        }
+
+        [Fact]
+        public void Ema_SimpleValues_Calculated()
+        {
+            //Given
+            var ma = new MA();
+            //When
+            var result = ma.Ema(data, 3);
+            double[] expected = { 44.3289, 44.2096, 44.1796, 43.896, 44.1119, 44.4692, 44.7823, 45.1034, 45.4734, 45.778, 45.8355, 45.9342, 45.7741, 46.028};
+            var result2 = result.Select(x => Math.Round(x.Value,4)).ToArray();
+            //Then
+            Assert.Equal(expected, result2);
         }
     }
 }
