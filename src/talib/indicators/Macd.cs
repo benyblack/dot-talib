c225e8fcacccbf9f4f6fcf8b6733b95dcc1a5a68
Behnam <benyblack@gmail.com>
2019-08-14 12:07:25 +0200

    Add MACD method
    It return a list of 3 values (macd, signal, histogram)
    The unit test fails and it seems the results
    in comparison to ta-lib in python are slightly different.
    This can be skiped but I prefer to furthur investigation.

--- src/talib/indicators/Macd.cs
+++ src/talib/indicators/Macd.cs
@@ -6,6 +6,8 @@ namespace Talib.Indicators
 {
     public class Macd
     {
+
+
         public static double? MacdSingle(double[] data, int fast = 12, int slow = 26)
         {
             var ma = new MA();
@@ -31,7 +33,23 @@ namespace Talib.Indicators
 
         public static double? HistogramSingle(double[] data, int signal_period = 9, int macd_fast = 12, int macd_slow = 26)
         {
-            return  MacdSingle(data, macd_fast, macd_slow) - SignalSingle(data, signal_period);
+            return MacdSingle(data, macd_fast, macd_slow) - SignalSingle(data, signal_period);
+        }
+
+        public static (double?[] macd, double?[] signal, double?[] histogram) MACD(double[] data, int signal_period = 9, int macd_fast = 12, int macd_slow = 26)
+        {
+            var macd_list = new List<double?>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                var selected_data = data.Take(data.Length - i).ToArray();
+                var current_macd = MacdSingle(selected_data, macd_fast, macd_slow);
+                var current_signal = SignalSingle(selected_data, signal_period, macd_fast, macd_slow);
+                var current_histogram = HistogramSingle(selected_data, signal_period, macd_fast, macd_slow);
+                macd_list.Add(current_macd);
+            }
+
+            macd_list.Reverse();
+            return (macd_list.ToArray(), null, null);
         }
     }
 }
